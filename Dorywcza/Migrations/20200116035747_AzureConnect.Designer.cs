abf62c7dbc6e23d7917a9c51f4be74e4b83aa9f4
Wojciech <wojciech.ligeza@gmail.com>
2020-01-16 05:00:17 +0100

    DockerComposeAdded

--- Dorywcza/Migrations/20200116035747_AzureConnect.Designer.cs
+++ Dorywcza/Migrations/20200116035747_AzureConnect.Designer.cs
@@ -4,13 +4,16 @@ using Dorywcza.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dorywcza.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200116035747_AzureConnect")]
+    partial class AzureConnect
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Dorywcza/Migrations/20200116035747_AzureConnect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dorywcza.Migrations
+{
+    public partial class AzureConnect : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "JobOffers",
+                keyColumn: "JobOfferId",
+                keyValue: 1,
+                column: "AddDate",
+                value: new DateTime(2020, 1, 16, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "JobOffers",
+                keyColumn: "JobOfferId",
+                keyValue: 1,
+                column: "AddDate",
+                value: new DateTime(2020, 1, 10, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
